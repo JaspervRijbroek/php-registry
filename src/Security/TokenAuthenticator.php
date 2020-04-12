82f3f7926d617505e8a9717103f91c74a291820c
Jasper van Rijbroek <jasper@sition.nl>
2020-04-12 14:56:45 +0200

    Added method stubs

--- src/Security/TokenAuthenticator.php
+++ src/Security/TokenAuthenticator.php
@@ -3,6 +3,11 @@
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 class TokenAuthenticator extends AbstractGuardAuthenticator
@@ -12,5 +17,61 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
         // If a token is send we are supported.
 
         return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // TODO: Implement start() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCredentials(Request $request)
+    {
+        // TODO: Implement getCredentials() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // TODO: Implement getUser() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // TODO: Implement checkCredentials() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // TODO: Implement onAuthenticationFailure() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsRememberMe()
+    {
+        // TODO: Implement supportsRememberMe() method.
     }
 }
